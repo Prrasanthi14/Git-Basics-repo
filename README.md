bf8bf9456ae2f749798590c6f7796d45fe0acdff
Prrasanthi14 <vspooj@gmail.com>
2022-10-31 20:43:22 +0530

    created rock-paper-scissors python file

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Git-Basics-repo" 
---
+++ rock_paper_scissors_game.py
@@ -0,0 +1,63 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)______
+          ________)
+       ____________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+
+user=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+
+complay=random.randint(0,2)
+
+print("\n\nYOU")
+if user==0:
+    print(rock)
+elif user==1:
+    print(paper)
+elif user==2:
+    print(scissors)
+
+print("\n\nCOM")
+if complay==0:
+  print(rock)
+elif complay==1:
+  print(paper)
+elif complay==2:
+  print(scissors)
+
+if user==0 and complay==2:
+  print("You win")
+elif user==1 and complay==0:
+  print("You win")
+
+elif user==2 and complay==1:
+  print("You win")
+
+elif user==complay:
+  print("Tie!!! Play again!")
+
+else:
+  print("You lose")
